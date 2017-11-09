811b08bd8c7e07ef904a4d721a7c7e6293a82a1a
Tony Milton <tonymilton@Tonys-MacBook-Pro.local>
2017-11-10 08:03:44 +1100

    Initial Commit

---
+++ rxSwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rxSwiftTest
+//
+//  Created by Tony Milton on 10/11/17.
+//  Copyright © 2017 Tony Milton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
